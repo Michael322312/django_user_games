3fef2a76cf014083dfa5e0e327794f236ff24896
Michael322312 <zomisha0808@gmail.com>
2024-02-02 12:14:09 +0200

    Added all actions into func's and created func that shows all games and their info

--- main.py
+++ main.py
@@ -1,40 +1,56 @@
 import django_connect
 from games.models import Game, Genre
 
-# genre_shooter = Genre(
-#     name="Shooter", 
-#     description="In this genre you need to kill enemies by shooting in them"
-# )
 
-# genre_advantures = Genre(
-#     name="Advantures",
-#     description="This genre is perfect for players that like to travel and solve mysteries in different places"
-# )
+def create_genres():
+    genre_shooter = Genre(
+        name="Shooter", 
+        description="In this genre you need to kill enemies by shooting in them"
+    )
 
-# hogwarts_legacy = Game(
-#     name="Hogwarts Legacy",
-#     year=2023,
-#     rate=9
-# )
+    genre_advantures = Genre(
+        name="Advantures",
+        description="This genre is perfect for players that like to travel and solve mysteries in different places"
+    )
 
-# destiny_2 = Game(
-#     name="Destiny 2",
-#     year=2017,
-#     rate=9
-# )
+    genre_shooter.save()
+    genre_advantures.save()
 
-# genre_shooter.save()
-# genre_advantures.save()
 
-# hogwarts_legacy.save()
-# destiny_2.save()
+def create_games():
+    hogwarts_legacy = Game(
+        name="Hogwarts Legacy",
+        year=2023,
+        rate=9
+    )
 
-hogwarts_legacy = Game.objects.filter(name="Hogwarts Legacy", id = 1).first()
-adventure_genre = Genre.objects.get(id=2)
+    destiny_2 = Game(
+        name="Destiny 2",
+        year=2017,
+        rate=9
+    )
 
-hogwarts_legacy.genre.add(adventure_genre)
+    hogwarts_legacy.save()
+    destiny_2.save()
 
-destiny_2 = Game.objects.get(id=2)
-shooter_genre = Genre.objects.get(id=1)
+def add_genres_to_games():
+    hogwarts_legacy = Game.objects.filter(name="Hogwarts Legacy", id = 1).first()
+    adventure_genre = Genre.objects.get(id=2)
 
-destiny_2.genre.add(shooter_genre)
+    hogwarts_legacy.genre.add(adventure_genre)
+
+    destiny_2 = Game.objects.get(id=2)
+    shooter_genre = Genre.objects.get(id=1)
+
+    destiny_2.genre.add(shooter_genre)
+
+
+def show_all_games():
+    games = Game.objects.all()
+    print("All games:\n")
+    for game in games:
+        print(f"{game.id}. Name: '{game.name}' Genre: '{game.genre}' Year: '{game.year}' Rate: '{game.rate}'")
+        print("\n")
+
+
+show_all_games()
