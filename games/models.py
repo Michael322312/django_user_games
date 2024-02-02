dc501d635425a23e7ba114bde473418e6d683ea2
Michael322312 <zomisha0808@gmail.com>
2024-02-02 11:46:07 +0200

    Created VirtualEnViorment and created app  with   and

--- games/models.py
+++ games/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Game(models.Model):
+    name = models.CharField(max_length=100)
+    genre = models.ManyToManyField("Genre", related_name="genres")
+    year = models.IntegerField()
+    rate = models.DecimalField(decimal_places=1, max_digits=10)
+
